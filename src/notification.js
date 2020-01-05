0cf1674b519801457369f14fb3d69f399dc56e8c
Juuso Lehtinen <juuso@karttalehtinen.fi>
2020-01-05 16:21:57 +0200

    remove notifications

--- src/notification.js
+++ src/notification.js
@@ -1,19 +1,7 @@
 export const requestPermissions = () => {
-  if (!Notification) {
-    return;
-  }
 
-  if (Notification.permission === 'default') {
-    Notification.requestPermission();
-  }
 };
 
 export const send = (text) => {
-  if (!Notification) {
-    return;
-  }
 
-  if (Notification.permission === 'granted') {
-    new Notification(text);
-  }
 };
