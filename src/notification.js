5185eb091592dfd92f30b4a587b1330408e5397d
Juuso Lehtinen <juuso@karttalehtinen.fi>
2020-01-05 16:17:37 +0200

    Don't try to notify if not possible

--- src/notification.js
+++ src/notification.js
@@ -1,10 +1,18 @@
 export const requestPermissions = () => {
+  if (!Notification) {
+    return;
+  }
+
   if (Notification.permission === 'default') {
     Notification.requestPermission();
   }
 };
 
 export const send = (text) => {
+  if (!Notification) {
+    return;
+  }
+
   if (Notification.permission === 'granted') {
     new Notification(text);
   }
